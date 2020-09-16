cf3cb9f5d379ff86afce897b001e061334f380e6
sagawashunsuke <shunkurin73@gmail.com>
2020-09-16 21:50:57 +0900

    モデルの単体テストコード再々修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
   validates :first_name
   validates :family_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'}do
   validates :family_name_kana
   validates :first_name_kana
   end
