3ccf680c20cad20a106d26101f4ee3081a082f72
sagawashunsuke <shunkurin73@gmail.com>
2020-09-19 12:16:04 +0900

    itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_day
   has_one_attached :image
   
-  # validates :category
-  # validates :delivery_fee
-  # validates :user
-  # validates :name
-  # validates :text
-  # validates :condition
-  # validates :price
-  # validates :shipping_area
-  # validates :shipping_day
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :text, presence: true, length: { maximum: 1000 }
+  validates :genre_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_day_id, numericality: { other_than: 1 }
+  validates :image, presence: true
+  validates :price, presence: true, format: { with:/\A[0-9]+\z/i, message: '半角英数字を使用してください' }
+  validates :price, inclusion: { in: 300..9999999 }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
+
   factory :item do
-    
+    name              {'テレビ'}
+    text              {'2019年製の黒いToshibaのテレビ'}
+    genre_id          {"2"}
+    delivery_fee_id   {'2'}
+    user_id           {'2'}
+    status_id         {'3'}
+    shipping_area_id  {'3'}
+    shipping_day_id   {'2'}
+    price             {'500'}
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
