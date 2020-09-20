bd0c7a76334e819aae021de7a2aec31f066c229a
sagawashunsuke <shunkurin73@gmail.com>
2020-09-20 20:35:57 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
   def move_to_index
     unless user_signed_in?
